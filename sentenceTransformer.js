5aa0d348ff69baf45be01d9c8226574b46f6ff65
arnav jain <jain.arnav@hotmail.com>
2023-09-08 07:12:39 -0700

    more code

--- sentenceTransformer.js
+++ sentenceTransformer.js
@@ -1,6 +1,7 @@
 const model_id = "sentence-transformers/all-MiniLM-L6-v2"
 const hf_token = "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"
 
+
 module.exports.createEmbedding = function(sentence){
     return sentence.length;
 } 
