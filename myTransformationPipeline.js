9f4a8ee714daca6d1bcbb1f7681b32786133d539
arnav jain <jain.arnav@hotmail.com>
2023-09-08 07:12:42 -0700

    Added offline processing.

---
+++ myTransformationPipeline.js
@@ -0,0 +1,20 @@
+import { pipeline, env } from '@xenova/transformers';
+
+class MyTransformationPipeline {
+  static task = 'feature-extraction';
+  static model = 'Xenova/all-MiniLM-L6-v2';
+  static instance = null;
+
+  static async getInstance(progress_callback = null) {
+    if (this.instance === null) {
+      // NOTE: Uncomment this to change the cache directory
+      // env.cacheDir = './.cache';
+
+      this.instance = pipeline(this.task, this.model, { progress_callback });
+    }
+
+    return this.instance;
+  }
+}
+
+export let myTransformationPipeline = await MyTransformationPipeline.getInstance()
