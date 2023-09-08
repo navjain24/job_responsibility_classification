80705ecaec04ec5e740c3c3795ae67cabbf17acc
arnav jain <jain.arnav@hotmail.com>
2023-09-08 07:12:42 -0700

    Added offline transformer

--- sentenceTransformer.js
+++ sentenceTransformer.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { myTransformationPipeline } from './myTransformationPipeline.js';
 
 const model_id = "sentence-transformers/all-MiniLM-L6-v2"
 const hf_token = "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"
@@ -14,7 +15,8 @@ async function getHuggingFaceEmbedding(sentence) {
     try {
         const texts = [sentence];
         const data = {"inputs": texts, "options":{"wait_for_model":"True"}};
-        const response = await axios.post(api_url, data, config);
+        //const response = await axios.post(api_url, data, config);
+        const response = await myTransformationPipeline(sentence);
         console.log(response.data);
         return response.data;
     } catch (error) {
