add19ca797bdde48c93aa005ac30134ed24d459b
arnav jain <jain.arnav@hotmail.com>
2023-09-08 07:12:43 -0700

    refactor and code cleanup

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-import puppeteer from 'puppeteer';
-import cheerio from 'cheerio';
 import fs from 'fs';
 
+import { scrapeRawData } from './jobExtractor.js';
 import * as st from './sentenceTransformer.js';
 import * as se from './sentenceExtractor.js';
 
 
-const INDEED_URL = 'https://www.indeed.com';
 const BRONZE_DATA = 'BRONZE_DATA';
 const SILVER_DATA = 'SILVER_DATA';
 const GOLD_DATA = 'GOLD_DATA';
@@ -32,92 +30,15 @@ async function processJobRole(jobRole, location) {
 
   //BRONZE
   //Scrape raw data from the URL
-  //await scrapeRawData(jobRole, location);
+  await scrapeRawData(jobRole, location);
 
   //SILVER
   //Extract responsibilities to Silver data folder
-  await extractResponsibilities(jobRole, location);
+  //await extractResponsibilities(jobRole, location);
 
   console.log(`Finished processing Job Role: ${jobRole}\nLocation: ${location}\n`);
 }
 
-async function scrapeRawData(jobRole, location) {
-  const searchUrl = `${INDEED_URL}/jobs?q=${jobRole}&l=${location}&sc=0kf:jt(fulltime);`;
-
-  const jobIds = await getJobIdsFromJobSearchSite(searchUrl);
-  console.log(`Found ${jobIds.length} job listings on the job search site.`);
-
-  // Write raw descriptions to file(s) (BRONZE)
-  for (const jobId of jobIds) {
-    try {
-      const rawDetail = await getRawJobDetailPage(jobRole, location, jobId);
-      console.log(`Job ID: ${jobId}\nRaw Detail: ${rawDetail}\n`);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  console.log(`Scraped raw data for Job Role: ${jobRole}; Location: ${location}`);
-}
-
-async function getJobIdsFromJobSearchSite(url) {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const pageData = await page.evaluate(() => {
-    return {
-      html: document.documentElement.innerHTML,
-    };
-  });
-
-  const $ = cheerio.load(pageData.html);
-
-  let mosaicJobResults = $('#mosaic-jobResults')
-  let jobCards = mosaicJobResults.find('div.mosaic-provider-jobcards');
-  let cardOutlines = jobCards.find('div.cardOutline');
-
-  jobIds = [];
-
-  cardOutlines.each((index, element) => {
-    //Job URL
-    let jobSeenBeacon = $(element).find('div.job_seen_beacon a');
-    let jobId = $(jobSeenBeacon).attr('data-jk');
-
-    console.log(jobId);
-
-    //Snippet
-    let jobSnippet = $(element).find('div.job-snippet');
-    responsibility = jobSnippet.text();
-    jobIds.push(jobId);
-  });
-
-  await browser.close();
-  return jobIds;
-}
-
-async function getRawJobDetailPage(jobRole, location, jobId) {
-  const url = `${INDEED_URL}/viewjob?jk=${jobId}`;
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const rawDetail = await page.evaluate(() => {
-    return document.documentElement.innerHTML;
-  });
-
-  await browser.close();
-
-  const rootFolder = BRONZE_DATA
-  await makeDirIfNeeded(rootFolder);
-
-  const parentFolder = `${jobRole}_${location}`;
-  const destinationFolder = `${rootFolder}/${parentFolder}`;
-  await makeDirIfNeeded(destinationFolder);
-
-  fs.writeFileSync(`${destinationFolder}/${jobId}.html`, rawDetail);
-  return rawDetail;
-}
 
 async function extractResponsibilities(jobRole, location) {
   const rootFolder = BRONZE_DATA
@@ -169,16 +90,6 @@ async function extractResponsibilitiesFromSingleJob(jobRole, location, fileName)
   });
 }
 
-async function makeDirIfNeeded(dirName) {
-  try {
-    if (!fs.existsSync(dirName)) {
-      fs.mkdirSync(dirName);
-    }
-  } catch (err) {
-    console.error(err);
-    throw err
-  }
-}
 
 async function getJobDescription(jobId) {
 }
